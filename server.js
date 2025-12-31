40acc396ddc6c1d8523e877ae1ea04ba0f5cf795
mahimakoirala <koiralamahima347@gmail.com>
2025-12-31 15:59:32 -0600

    Fix CORS issue

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://email-signature-teamenoch.onrender.com', 'http://localhost:3000'],
+  methods: ['GET', 'POST'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.static('public'));
 
